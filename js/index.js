2c2ca20d398de43d5a4dc1e1845be972965cd620
Josh Devine <joshuaddevine@gmail.com>
2021-11-01 18:18:54 -0400

    removed previous minting attempt

--- js/index.js
+++ js/index.js
@@ -25,34 +25,3 @@ window.setInterval(() => {
     nft1.src = nft_urls[i1];
     nft2.src = nft_urls[i2];
 }, 3000);
-
-
-const ethereumButton = document.getElementById('link');
-const sendEthButton = document.getElementById('mint');
-
-let accounts = [];
-
-//Sending Ethereum to an address
-sendEthButton.addEventListener('click', () => {
-  ethereum
-    .request({
-      method: 'eth_sendTransaction',
-      params: [
-        {
-          from: accounts[0],
-          to: '0xc8BE7c724AD291a90EceF75dEfE421703C02189b',
-          value: '',
-        },
-      ],
-    })
-    .then((txHash) => console.log(txHash))
-    .catch((error) => console.error);
-});
-
-ethereumButton.addEventListener('click', () => {
-  getAccount();
-});
-
-async function getAccount() {
-  accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-}
